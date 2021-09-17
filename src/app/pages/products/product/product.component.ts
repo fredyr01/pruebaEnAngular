0f170b1e95165329786a8270a537a4154aadc4aa
fredyr01 <fredyrios01@gmail.com>
2021-09-17 01:02:19 -0500

    Subo contenido realizado en la segunda hora hasta 1hr y 50 min

--- src/app/pages/products/product/product.component.ts
+++ src/app/pages/products/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Product } from '../interfaces/product.interface';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  @Input()
+  product!: Product;
+
+  @Output() addToCartClick = new EventEmitter<Product>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(): void{
+    // console.log('Click', this.prodct)
+    this.addToCartClick.emit(this.product);
+
   }
 
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from './services/products.service';
-import {tap} from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+import { Product } from './interfaces/product.interface';
 
 @Component({
   selector: 'app-products',
@@ -8,15 +9,19 @@ import {tap} from 'rxjs/operators';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  products!: Product[];
   constructor(private productSvc: ProductsService) { }
 
   ngOnInit(): void {
     this.productSvc.getProducts()
     .pipe(
-      tap( res => console.log(res))
+      tap( (products: Product [] )=> this.products = products)
     )
     .subscribe();
+  }
+
+  addToCart(product: Product):  void{
+    // console.log('Add to Cart', product);
   }
 
 }
